fb3cece690542d96d6f32f4a639c4c9b727d465d
Dmitriy Karpovich <dmkrch1@gmail.com>
2020-04-13 12:59:19 +0300

    added 3-5 lab functionality

---
+++ lab6/Surgeon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace doctor
+{
+    class Surgeon : Doctor
+    {
+        public Surgeon(string name, int age, int workExperience, string qualification, string scienceDegree)
+        {
+            Name = name;
+            Age = age;
+            WorkExperience = workExperience;
+            Qualification = qualification;
+            ScienceDegree = scienceDegree;
+
+            ++_counter;
+            Id = _counter;
+        }
+
+        public int Id { get; set; }
+
+        public void AddOperation(Operation operation)
+        {
+                operationsList.Add(operation);
+        }
+
+        public void ShowAllOperations()
+        {
+            for (int i = 0; i < operationsList.Count; ++i)
+            {
+                Console.WriteLine("\tTITLE: " + operationsList[i].Title + "\tDIFFICULTY: " + operationsList[i].Difficulty + "\n\tSUCCESSED: " + operationsList[i].SuccessOperations + "\t\tCOMPLICATED: " + operationsList[i].ComplicatedOperations);
+            }
+        }
+
+        public double GetOperationPercentage(string title)
+        {
+            double percentage = -1;
+
+            for (int i = 0; i < operationsList.Count; ++i)
+            {
+                if (operationsList[i].Title == title)
+                {
+                    percentage = operationsList[i].SuccessOperations / (operationsList[i].SuccessOperations + operationsList[i].ComplicatedOperations);
+                }
+            }
+
+            return percentage;
+        }
+
+        public void AddStatisticOperation(string title, bool state)
+        {
+            for (int i = 0; i < operationsList.Count; ++i)
+            {
+                if (operationsList[i].Title == title)
+                {
+                    if (state == true)
+                    {
+                        operationsList[i].SuccessOperations++;
+                    }
+                    else
+                    {
+                        operationsList[i].ComplicatedOperations++;
+                    }
+                }
+            }
+        }
+
+
+        private static int _counter = -1;
+        private List<Operation> operationsList = new List<Operation>();
+    }
+}
---
+++ lab6/SurgeonList.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace doctor
+{
+    class SurgeonList
+    {
+        public void InitSurgeons()
+        {
+            string[] fileInput = File.ReadAllLines("surgeons.txt");
+
+
+            for (int i = 0; fileInput[i] != "eof";)
+            {
+                string name = fileInput[i++];
+                int age = Int32.Parse(fileInput[i++]);
+                int workExperience = Int32.Parse(fileInput[i++]);
+                string qualification = fileInput[i++];
+                string scienceDegree = fileInput[i++];
+
+                Surgeon surgeon = new Surgeon(name, age, workExperience, qualification, scienceDegree);
+
+               
+                while (fileInput[i++] == "///////////////")
+                {
+                    string title = fileInput[i++];
+                    string difficulty = fileInput[i++];
+                    int successedOp = Int32.Parse(fileInput[i++]);
+                    int complicatedOp = Int32.Parse(fileInput[i++]);
+
+                    Operation op = new Operation(title, difficulty, successedOp, complicatedOp);
+                    surgeon.AddOperation(op);
+                }
+
+                surgeonList.Add(surgeon);
+
+            }
+        }
+
+        public void ShowAllInfo(int id)
+        {
+            Console.WriteLine("#" + (surgeonList[id].Id + 1) + "      NAME: " + surgeonList[id].Name + "\t\tAGE: " + surgeonList[id].Age + "\n\tWORK EXPERIENCE: " + surgeonList[id].WorkExperience + "\tQUALIFICATION: " + surgeonList[id].Qualification);
+            Console.WriteLine("\n");
+            Console.WriteLine("\n\t" + surgeonList[id].Name + "'s operations:\n");
+            surgeonList[id].ShowAllOperations();
+        }
+
+        public string GetName(int id)
+        {
+            return surgeonList[id].Name;
+        }
+
+        public int FindBestSurgeon(string title)
+        {
+            int id = -1;
+            double currentPercentage = -1;
+            double maxPercentage = -1;
+
+            for (int i = 0; i < surgeonList.Count; ++i)
+            {
+                currentPercentage = surgeonList[i].GetOperationPercentage(title);
+
+                if (currentPercentage > maxPercentage)
+                {
+                    maxPercentage = currentPercentage;
+                    id = i;
+                }
+            }
+
+            return id;
+        }
+
+        public int PerformOperation()
+        {
+            int result = -1;
+            Random rnd = new Random();
+
+
+            Console.Write("What operation do u need to perform? ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Which doctor you want to choose? ");
+            int id = Int32.Parse(Console.ReadLine()) - 1;
+
+            if (surgeonList[id].GetOperationPercentage(title) != -1)
+            {
+                Console.Write("The doctor " + surgeonList[id].Name + " has " + surgeonList[id].GetOperationPercentage(title) * 100 + " % on success.\nDo u want to perform the operation?(1 - yes, 2 - no)  ");
+                int choice = Int32.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    int number = 0;
+
+                    number = rnd.Next(1, 101);
+
+                    if (number <= (int)(surgeonList[id].GetOperationPercentage(title) * 100 + 1))
+                    {
+                        Console.Clear();
+                        for (int i = 0; i < 20; ++i)
+                        {
+                            Console.Write(".");
+                            System.Threading.Thread.Sleep(200);
+                        }
+
+                        surgeonList[id].AddStatisticOperation(title, true);
+
+                        result = 1;
+                    }
+
+                    else
+                    {
+                        Console.Clear();
+                        for (int i = 0; i < 20; ++i)
+                        {
+                            Console.Write(".");
+                            System.Threading.Thread.Sleep(200);
+                        }
+
+                        surgeonList[id].AddStatisticOperation(title, false);
+
+                        result = 0;
+                    }
+                }
+
+                else
+                {
+                    result = -1;
+                }
+
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("This doctor does not perform such operations\n");
+                return -2;
+            }
+        }
+
+        private List <Surgeon> surgeonList = new List <Surgeon>();
+    }
+}
